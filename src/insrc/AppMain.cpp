2b32622e7b1e12a7de22c68f5ac35c49653c2b0e
TheOathMan <57967535+TheOathMan@users.noreply.github.com>
2022-04-03 13:09:56 +0300

    Update AppMain.cpp

--- src/insrc/AppMain.cpp
+++ src/insrc/AppMain.cpp
@@ -1,29 +1,29 @@
 #include "../outsrc/imgui/imgui.h"
 #include "../outsrc/GLFW/glfw3.h"
 #include "app_window.h"
-#include <iostream>
 
 // Runs once before the creation of the window
-void AppAwake(App_Window* window){
+void AppAwake(App_Window* window)
+{
 
 }
 
 
-
-void AppStart(App_Window* window){
-    //glfwSetMouseButtonCallback(window->get_window(),[](GLFWwindow*,int a,int b,int c){});
-    //glfwSetCursorPosCallback (window->get_window(),[](GLFWwindow*w,double,double){ });
+// Runs at the start of the app
+void AppStart(App_Window* window)
+{
 
 }
 
 // Runs everyfram
-void AppUpdate(App_Window* window, float deltaTime){
+void AppUpdate(App_Window* window, float deltaTime)
+{
     ImGui::ShowDemoWindow();
-
 }
 
 
 // Runs once when app is closed
-void AppClosed(App_Window* window){
+void AppClosed(App_Window* window)
+{
 
 }
