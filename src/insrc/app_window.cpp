c1214e013a18a324feab5992f57198f083830196
TheOathMan <57967535+TheOathMan@users.noreply.github.com>
2022-04-03 13:08:00 +0300

    Update app_window.cpp

--- src/insrc/app_window.cpp
+++ src/insrc/app_window.cpp
@@ -1,5 +1,15 @@
-#include "app_window.h"
+#include "app_window.h" 
 #include <stdio.h>
+#include "../outsrc/imgui/imgui_impl_glfw.h"
+#include "../outsrc/imgui/imgui_impl_opengl2.h"
+#include "../outsrc/imgui/imgui_internal.h"
+
+//thread_local ImGuiContext* MyImGuiTLS; // thread local context if need. ned to be activated from imconfig.h
+
+void AppAwake(App_Window* window);
+void AppStart(App_Window* window);
+void AppUpdate(App_Window* window, float deltaTime);
+void AppClosed(App_Window* window);
 
 std::vector<App_Window*> App_Window::wins32;
 
@@ -8,7 +18,8 @@ App_Window::App_Window(const char* window_name, int width, int height, GLFWwindo
 	width(width),
 	height(height)
 {
-   // glfwSetErrorCallback(glfw_error_callback);
+    AppAwake(this);
+
     window = glfwCreateWindow(width, height, windowName, NULL, srd_window);
 
     if (!window) {
@@ -23,22 +34,35 @@ App_Window::App_Window(const char* window_name, int width, int height, GLFWwindo
     SetBackgroundColor(0.7f,0.7f,0.7f);
 }
 
-App_Window:: ~App_Window() 
-{
-    glfwDestroyWindow(window);
-}
-
 void App_Window::OnWindowAwake()
 {
-
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL2_Init();
+    AppStart(this);
 }
+
 
 
 void App_Window::OnUpdate()
 {
     glfwMakeContextCurrent(window);
-    glClear(GL_COLOR_BUFFER_BIT);
-    glClearColor(background_col[0],background_col[1],background_col[2], 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+    glClearColor(0.0f,0.0f,0.0f,0.0f);
+    //glfwSetWindowOpacity(window, 0.0f);
+
+    //frame start.
+    ImGui_ImplOpenGL2_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+
+
+    AppUpdate(this,ImGui::GetIO().DeltaTime);
+
+    //frame end.
+    ImGui::Render();
+    ImGui_ImplOpenGL2_RenderDrawData(ImGui::GetDrawData());
 }
 
 void App_Window::OnInput()
@@ -46,12 +70,17 @@ void App_Window::OnInput()
 
 }
 
+
+App_Window:: ~App_Window() 
+{
+    glfwDestroyWindow(window);
+    AppClosed(this);
+}
 void App_Window::SetBackgroundColor(float red, float green, float blue)
 {
     background_col[0] = red;
     background_col[1] = green;
     background_col[2] = blue;
-
 }
 
 // draw textured quad opengl
@@ -73,5 +102,4 @@ void RawQuadTexture(int id)
     glTexCoord2f(1.0f, 0.0f);
     glVertex2f(1.0f, 1.0f);
     glEnd();
-
 }
